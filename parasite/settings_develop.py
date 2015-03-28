e16378438cbd140ae18e048ea712253e78457d33
Daisuke Hashimoto <dsk.hashimoto@gmail.com>
2015-03-28 23:36:08 +0900

    develop

---
+++ parasite/settings_develop.py
@@ -0,0 +1,16 @@
+
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.mysql',
+        'NAME': 'parasite',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'OPTIONS': {
+           "init_command": "SET storage_engine=MYISAM",
+        }
+    }
+}
